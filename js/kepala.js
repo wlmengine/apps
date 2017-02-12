125d93b4f21af6fb945bb7722239a60f867a8c47
wlmengine <wlmengine@users.noreply.github.com>
2017-02-12 21:43:36 +0700

    Update kepala.js

--- js/kepala.js
+++ js/kepala.js
@@ -1,1 +1,50 @@
-var myVariable = '\t<meta charset=\"utf-8\">\n\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta property=\"og:image\" content=\"https:\/\/flic.kr\/p\/pHohZH\"\/>\n    <meta property=\"og:type\" content=\"website\"\/>\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <meta name=\"description\" content=\"Personal website\">\n    <meta name=\"author\" content=\"William Yap\">\n    <link rel=\"icon\" href=\"https:\/\/williamyaps.github.io\/wlmjavascript\/img\/wlm.ico\">\n\t<link rel=\"stylesheet\" href=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/font-awesome\/4.7.0\/css\/font-awesome.min.css\">\n\t<link rel=\"stylesheet\" href=\"https:\/\/fonts.googleapis.com\/icon?family=Material+Icons\">\n\t<base target=\"_parent\">\n\t<bgsound src=\"https:\/\/priskilapeh.github.io\/files\/GiveThanks.mp3\" loop=\"-1\">\n\t\n\t\t<title>叶威廉® William Yap Personal Sites<\/title>\n\t\t<meta http-equiv=\"Content-Type\" content=\"text\/html; charset=gb2312\">\n\t\t<meta name=\"viewport\" content=\"initial-scale=1.0, minimum-scale=1, maximum-scale=1\">\t\n\t\t<script src=\"https:\/\/williamyaps.github.io\/js\/jquery-1.6.min.js\"><\/script>\n\t\t<script src=\"https:\/\/williamyaps.github.io\/js\/jquery.touchSwipe-1.2.4.js\"><\/script>\n\t\t<script src=\"https:\/\/williamyaps.github.io\/js\/html5.js\"><\/script>    \n\t\t\n\t\t<link href=\"css\/style.css\" rel=\"stylesheet\">\t\t\n\t\t<\/style>\t\n<base target=\"_parent\">\n<bgsound src=\"https:\/\/priskilapeh.github.io\/files\/GiveThanks.mp3\" loop=\"-1\">\n<\/head>\n<body background=\"air1.gif\" style=\"background-attachment: fixed\">\n\n<script type=\"text\/javascript\">\n(function(){\n\tvar hexacode = [\'#ffff00\', \'#ff0000\', \'#00ff00\', \'#0000ff\', \'#ffffff\', \'#008000\', \'#CCFFFF\', \'#CCCCFF\', \'#FFCCFF\', \'#FFCCCC\', \'#66FFCC\', \'#75DEF0\'],\n\tel = document.getElementById(\'autocolor\').style,\n\tcounter = -1,\n\thexalen = hexacode.length;\n\tfunction auto(){\n\t\tel.backgroundColor = hexacode[counter = ++counter % hexalen];\n\t\tel.fontColor = hexacode[counter = ++counter % hexalen];\n\t}\n\tsetInterval(auto, 1000);\n})();\n<\/script>\n\n<br>\n<div style=\"text-align:center;\">\n<iframe name=\"I1\" height=\"700\" width=\"100%\" style=\"border:2px solid grey;\" title=\"Priskila Peh Photos Editor\" border=\"0\" frameborder=\"0\" noresize src=\"https:\/\/www.magisto.com\/create\" scrolling=\"no\" marginheight=\"30\" marginwidth=\"20\" vertical-align:bottom>\nYour browser does not support inline frames or is currently configured not to display inline frames.\n<\/iframe>\n<\/div>\n<embed src=\"https:\/\/priskilapeh.github.io\/files\/GiveThanks.mp3\" width=\"0\" height=\"0\" autoplay=\"true\" loop=\"true\">';
+
+var myvar = '<meta charset="utf-8">'+
+'	<meta http-equiv="X-UA-Compatible" content="IE=edge">'+
+'    <meta property="og:image" content="https://flic.kr/p/pHohZH"/>'+
+'    <meta property="og:type" content="website"/>'+
+'    <meta name="viewport" content="width=device-width, initial-scale=1">'+
+'    <meta name="description" content="Personal website">'+
+'    <meta name="author" content="William Yap">'+
+'    <link rel="icon" href="https://williamyaps.github.io/wlmjavascript/img/wlm.ico">'+
+'	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">'+
+'	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">'+
+'	<base target="_parent">'+
+'	<bgsound src="https://priskilapeh.github.io/files/GiveThanks.mp3" loop="-1">'+
+'	'+
+'		<title>???® William Yap Personal Sites</title>'+
+'		<meta http-equiv="Content-Type" content="text/html; charset=gb2312">'+
+'		<meta name="viewport" content="initial-scale=1.0, minimum-scale=1, maximum-scale=1">	'+
+'		<script src="https://williamyaps.github.io/js/jquery-1.6.min.js"></script>'+
+'		<script src="https://williamyaps.github.io/js/jquery.touchSwipe-1.2.4.js"></script>'+
+'		<script src="https://williamyaps.github.io/js/html5.js"></script>    '+
+'		'+
+'		<link href="css/style.css" rel="stylesheet">		'+
+'		</style>	'+
+'<base target="_parent">'+
+'<bgsound src="https://priskilapeh.github.io/files/GiveThanks.mp3" loop="-1">'+
+'</head>'+
+'<body background="air1.gif" style="background-attachment: fixed">'+
+''+
+'<script type="text/javascript">'+
+'(function(){'+
+'	var hexacode = [\'#ffff00\', \'#ff0000\', \'#00ff00\', \'#0000ff\', \'#ffffff\', \'#008000\', \'#CCFFFF\', \'#CCCCFF\', \'#FFCCFF\', \'#FFCCCC\', \'#66FFCC\', \'#75DEF0\'],'+
+'	el = document.getElementById(\'autocolor\').style,'+
+'	counter = -1,'+
+'	hexalen = hexacode.length;'+
+'	function auto(){'+
+'		el.backgroundColor = hexacode[counter = ++counter % hexalen];'+
+'		el.fontColor = hexacode[counter = ++counter % hexalen];'+
+'	}'+
+'	setInterval(auto, 1000);'+
+'})();'+
+'</script>'+
+''+
+'<br>'+
+'<div style="text-align:center;">'+
+'<iframe name="I1" height="700" width="100%" style="border:2px solid grey;" title="Priskila Peh Photos Editor" border="0" frameborder="0" noresize src="https://www.magisto.com/create" scrolling="no" marginheight="30" marginwidth="20" vertical-align:bottom>'+
+'Your browser does not support inline frames or is currently configured not to display inline frames.'+
+'</iframe>'+
+'</div>'+
+'<embed src="https://priskilapeh.github.io/files/GiveThanks.mp3" width="0" height="0" autoplay="true" loop="true">';
+	
